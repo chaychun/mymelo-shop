4674d89196fbcf9a565c734e8305ea9517c37470
1Krista <puddingme@yahoo.com>
2025-02-21 18:23:43 +0700

    Improve: increase size of search bubble and search bar for mobile

--- src/components/SearchBubble.tsx
+++ src/components/SearchBubble.tsx
@@ -63,11 +63,14 @@ function SearchBubble({ activeFilter, searchTerm, setSearchTerm }: PropsType) {
     return (
       <div
         className={cn(
-          "flex items-center gap-3 bg-neutral-100 border-neutral-300 rounded-lg px-2 h-11 shadow-xl border-none focus-within:border-neutral-400",
+          "flex items-center gap-3 bg-neutral-100 border-1 border-neutral-300 rounded-lg px-2 h-16 two-column:h-11 shadow-xl focus-within:border-neutral-400 two-column:border-none",
           className,
         )}
       >
-        <Search color="var(--color-neutral-400)" />
+        <Search
+          color="var(--color-neutral-400)"
+          size={isMulticolumn ? 24 : 32}
+        />
         <Input
           ref={inputRef}
           placeholder="Search..."
@@ -82,14 +85,14 @@ function SearchBubble({ activeFilter, searchTerm, setSearchTerm }: PropsType) {
           }}
           onBlur={searchTerm === "" ? toggleExpand : () => {}}
           onKeyDown={handleKeyDown}
-          className="border-none shadow-none p-0 focus-visible:outline-none focus-visible:ring-transparent"
+          className="border-none shadow-none p-0 focus-visible:outline-none focus-visible:ring-transparent text-lg"
         />
       </div>
     );
   }
 
   if (isMulticolumn) {
-    return searchBox("w-88");
+    return searchBox("w-88 text-base");
   } else {
     return (
       <div className="fixed bottom-4 right-4 flex items-center gap-2 z-1">
@@ -97,7 +100,7 @@ function SearchBubble({ activeFilter, searchTerm, setSearchTerm }: PropsType) {
           {isExpanded && (
             <motion.form
               initial={{ width: 0, opacity: 0 }}
-              animate={{ width: "calc(100vw - 6.25rem)", opacity: 1 }}
+              animate={{ width: "calc(100vw - 7.5rem)", opacity: 1 }}
               exit={{ width: 0, opacity: 0 }}
               transition={{ duration: 0.2 }}
             >
@@ -108,11 +111,11 @@ function SearchBubble({ activeFilter, searchTerm, setSearchTerm }: PropsType) {
         <motion.button
           whileTap={{ scale: 0.95 }}
           onClick={toggleExpand}
-          className="z-1 flex items-center justify-center w-11 h-11 text-white bg-primary rounded-lg shadow-lg focus:outline-none hover:bg-neutral-800 transition-colors cursor-pointer"
+          className="z-1 flex items-center justify-center w-16 h-16 text-white bg-primary rounded-lg shadow-lg focus:outline-none hover:bg-neutral-800 transition-colors cursor-pointer"
         >
           {isExpanded ?
-            <X size={24} />
-          : <Search size={24} />}
+            <X size={32} />
+          : <Search size={32} />}
         </motion.button>
       </div>
     );
