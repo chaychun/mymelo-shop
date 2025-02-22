3fea7f2ed6c2a13d570e53d44afba8f837d33c2a
1Krista <puddingme@yahoo.com>
2025-02-22 17:01:57 +0700

    Fix: wrapped app in cart provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import ProductList from "./components/ProductList";
 import { ProductsProvider } from "./contexts/ProductsContext";
+import { CartProvider } from "./contexts/CartContext";
 
 function App() {
   return (
     <ProductsProvider>
-      <main className="h-full p-20 scrollbar-hide">
-        <ProductList />
-      </main>
+      <CartProvider>
+        <main className="h-full p-20 scrollbar-hide">
+          <ProductList />
+        </main>
+      </CartProvider>
     </ProductsProvider>
   );
 }
